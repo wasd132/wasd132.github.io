68a7d006ca626bb430d46375ebe2fa9a9e25b248
YooYoungChan <wasd13579@gmail.com>
2026-06-25 20:28:26 +0900

    Remove blog section from portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 
 $(document).ready(function() {
     general_utils();
-    blog_posts();
 })
 
 
@@ -16,74 +15,4 @@ function general_utils() {
 			width: $(this).attr('data-percent')
 		}, 1000);
 	});
-}
-
-function blog_posts() {
-
-    // keeping it static, can be fetched from a blog dynamically as well
-    let posts = [
-        {
-            url: 'https://velog.io/@wasd1324/posts',
-            title: '주간 개발 일지',
-        },
-        {
-            url: 'https://velog.io/@wasd132/n8n-%EC%9D%84-%ED%99%9C%EC%9A%A9%ED%95%9C-SOAR%EA%B5%AC%EC%B6%95',
-            title: 'n8n을 활용한 SOAR 구축',
-        },
-    ];
-
-    let post_html = [];
-
-    for(let post of posts) {
-
-        let tags;
-        
-        if(post.tags) {
-            tags = post.tags.map(tag => {
-                return `<a href="https://www.nagekar.com/tags#${tag}">${tag}</a>`
-            })
-        }
-
-        let post_template = `
-        <div class="blog-post" onclick="blog_link_click('${post.url}');">
-
-            <div class="blog-link">
-    
-                <h3><a href="${post.url}">${post.title}</a></h3>            
-
-            </div>
-    
-            <div class="blog-goto-link">
-                <img class="blog-arrow" src="/assets/images/right-open-mini.svg"/>
-            </div>
-        </div>
-        `;
-
-        post_html.push(post_template);
-    }
-
-    // for the more posts link
-    let post_template = `
-    <div class="blog-post more-blogs" onclick="blog_link_click('https://velog.io/@wasd132/posts');">
-
-        <div class="blog-link">
-
-            <h3><a href="https://velog.io/@wasd132/posts">Visit the blog for more posts</a></h3>            
-
-        </div>
-
-        <div class="blog-goto-link">
-            <img class="blog-arrow" src="/assets/images/right-open-mini.svg"/>
-        </div>
-    </div>
-    `;
-
-    post_html.push(post_template);
-
-    $('#rss-feeds').html(post_html);
-
-}
-
-function blog_link_click(url) {
-    window.location = url;
 }
